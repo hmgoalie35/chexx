5bb4eb125399db96d8165b9faf32d6b4faeab62b
Harris Pittinsky <harris@pittinsky.com>
2020-04-09 23:03:38 -0400

    Add training csv

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,11 @@
-import cv2 as cv
+import glob
+import xml.etree.ElementTree as ET
+from pathlib import Path
 
-from conf import IMG_DIR, TRAINING_DIR, VIDEO_DIR
+import cv2 as cv
+import pandas as pd
+
+from conf import IMG_DIR, TRAINING_DIR
 from readers.base import BaseReader
 
 
@@ -20,6 +25,27 @@ class Video2Img(BaseReader):
             self.written += 1
 
 
+def xml_to_csv(path, out_file):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            fname = Path('../media/img/training/source') / root.find('filename').text
+            value = (str(fname),
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    xml_df.to_csv(out_file, index=None)
+
+
 if __name__ == '__main__':
-    t = Video2Img(file_name=VIDEO_DIR / '20200408_223812.mp4')
-    t.run()
+    xml_to_csv(str(TRAINING_DIR / 'xml'), str(TRAINING_DIR / 'csv' / 'output.csv'))
