8153d7813d0db816e9db29fa6ac3c9fb1fca417a
Harris Pittinsky <harris@pittinsky.com>
2020-04-11 13:15:00 -0400

    Clean up video2img util

--- src/utils/video2img.py
+++ src/utils/video2img.py
@@ -1,20 +1,25 @@
+from pathlib import Path
+
 import cv2 as cv
 
-from conf import IMG_DIR, TRAINING_DIR
 from readers.base import BaseReader
 
 
 class Video2Img(BaseReader):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, label: str, output_dir: Path, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.label = label
+        self.output_dir = output_dir
         self.count = 0
-        self.written = 375
+        self.written = 0
         self.max = 500
+        self.frequency = 1
 
     def handle_frame(self, frame):
         self.count += 1
 
-        if self.written < self.max and self.count % 1 == 0:
-            f = IMG_DIR / TRAINING_DIR / 'source' / f'puck{self.written}.jpg'
-            cv.imwrite(str(f), frame)
+        if self.written < self.max and self.count % self.frequency == 0:
+            file_name = str(self.output_dir / f'{self.label}{self.written}.jpg')
+            cv.imwrite(file_name, frame)
             self.written += 1
+            print(f'Wrote frame to {file_name}')
