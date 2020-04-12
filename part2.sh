bb520516f6878955009ad0b6174e7a085b1a16ab
mpittinsky <mpittinsky@yext.com>
2020-04-12 12:31:01 -0400

    use opencv dnn example to run tensorflow model

--- part2.sh
+++ part2.sh
@@ -14,4 +14,6 @@ python object_detection/export_inference_graph.py \
   --trained_checkpoint_prefix ../train/model.ckpt-1600 \
   --output_directory ../fine_tuned_model
 
-cp -r fine_tuned_model/* $DIR/data/model
+cp -r ../fine_tuned_model/* $DIR/data/model
+
+python src/tf_text_graph_ssd.py --input=data/model/frozen_inference_graph.pb --output=data/model/graph.pbtxt --config=src/ssd_mobilenet_v2_coco.config
--- src/detect.py
+++ src/detect.py
@@ -4,25 +4,36 @@ from conf import DATA_IMG_DIR, DATA_MODEL_DIR
 
 
 def detect():
-    cvNet = cv.dnn.readNetFromTensorflow(
-        str(DATA_MODEL_DIR / 'frozen_inference_graph.pb'),
-        str(DATA_MODEL_DIR / 'graph.pbtxt')
-    )
+    # Jean Vitor de Paulo Blog - https://jeanvitor.com/tensorflow-object-detecion-opencv/
 
-    img = cv.imread(str(DATA_IMG_DIR / 'puck8.jpg'))
-    rows = img.shape[0]
-    cols = img.shape[1]
-    cvNet.setInput(cv.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
-    cvOut = cvNet.forward()
+    # Load a model imported from Tensorflow
+    tensorflowNet = cv.dnn.readNetFromTensorflow(str( DATA_MODEL_DIR / 'frozen_inference_graph.pb' ), str( DATA_MODEL_DIR / 'graph.pbtxt' ))
 
-    for detection in cvOut[0, 0, :, :]:
+    # Input image
+    img = cv.imread(str(DATA_IMG_DIR / 'puck50.jpg'))
+    rows, cols, channels = img.shape
+
+    # Use the given image as input, which needs to be blob(s).
+    tensorflowNet.setInput(cv.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
+
+    # Runs a forward pass to compute the net output
+    networkOutput = tensorflowNet.forward()
+
+    # Loop on the outputs
+    for detection in networkOutput[0,0]:
+
         score = float(detection[2])
-        if score > 0.3:
+        if score > 0.2:
+
             left = detection[3] * cols
             top = detection[4] * rows
             right = detection[5] * cols
             bottom = detection[6] * rows
-            cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
 
-    cv.imshow('img', img)
+            #draw a red rectangle around detected objects
+            cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (0, 0, 255), thickness=2)
+
+    # Show the image with a rectagle surrounding the detected objects
+    cv.imshow('Image', img)
     cv.waitKey()
+    cv.destroyAllWindows()
