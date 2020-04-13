708fb8ea768031b9a61c82899585db830bd0e2a5
Harris Pittinsky <harris@pittinsky.com>
2020-04-12 22:41:40 -0400

    Allow config via constructor

--- src/utils/video2img.py
+++ src/utils/video2img.py
@@ -1,25 +1,27 @@
-from pathlib import Path
-
 import cv2 as cv
 
 from readers.base import BaseReader
 
 
 class Video2Img(BaseReader):
-    def __init__(self, label: str, output_dir: Path, *args, **kwargs):
+    def __init__(self, label, output_dir, max_images, frequency, offset=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.label = label
         self.output_dir = output_dir
-        self.count = 0
-        self.written = 0
-        self.max = 500
-        self.frequency = 1
+        self.offset = offset
+        self.written_images = self.offset
+        self.max_images = max_images
+        self.frequency = frequency
+        self._count = 0
 
     def handle_frame(self, frame):
-        self.count += 1
+        self._count += 1
 
-        if self.written < self.max and self.count % self.frequency == 0:
-            file_name = str(self.output_dir / f'{self.label}{self.written}.jpg')
+        if (self.written_images - self.offset) < self.max_images and self._count % self.frequency == 0:
+            file_name = str(self.output_dir / f'{self.label}{self.written_images}.jpg')
             cv.imwrite(file_name, frame)
-            self.written += 1
+            self.written_images += 1
             print(f'Wrote frame to {file_name}')
+            return True
+        elif (self.written_images - self.offset) > self.max_images:
+            return False
