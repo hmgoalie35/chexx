acb4ba9c3095cc6756745163663648c92ec32f8a
Harris Pittinsky <harris@pittinsky.com>
2020-04-12 22:37:52 -0400

    gitignore dirs that are getting too big for git

--- src/conf.py
+++ src/conf.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_DIR = BASE_DIR / 'media'
-VIDEO_DIR = MEDIA_DIR / 'video'
+VIDEOS_DIR = MEDIA_DIR / 'videos'
 
 DATA_DIR = BASE_DIR / 'data'
 DATA_IMG_DIR = DATA_DIR / 'img'
