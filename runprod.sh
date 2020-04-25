d4525f116be8efca6fe99d9f7b5602d0eacd934b
Harris Pittinsky <harris@pittinsky.com>
2020-04-25 16:28:12 -0400

    git pull in runprod

--- runprod.sh
+++ runprod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-source venv/bin/activate && gunicorn -w 1 -t 10000000 --bind 0.0.0.0:5000 app
+git pull && source venv/bin/activate && gunicorn -w 1 -t 10000000 --bind 0.0.0.0:5000 app
