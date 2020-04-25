b690c31c9e9465de99eb4808a0a1559b9492974e
Harris Pittinsky <harris@pittinsky.com>
2020-04-25 16:02:58 -0400

    Add latest model

--- src/run.py
+++ src/run.py
@@ -1,7 +1,8 @@
 from conf import DATA_IMG_DIR, DATA_XML_DIR, VIDEOS_DIR
+from trackers.chexx import ChexxTracker
 from utils.video import Video2Img
 from utils.xml import strip_absolute_path
-from trackers.chexx import ChexxTracker
+
 
 if __name__ == '__main__':
     v2i = Video2Img(
@@ -15,4 +16,4 @@ if __name__ == '__main__':
     strip_absolute_path(input_dir=DATA_XML_DIR)
 
     ct = ChexxTracker(input_file=str(VIDEOS_DIR / 'chexx_couch_view.mp4'))
-    ct.run()
+    # ct.run()
