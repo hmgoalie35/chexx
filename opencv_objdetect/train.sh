cd83ded898c8fe1c162a32e33c9cbf32bf129513
Harris Pittinsky <harris@pittinsky.com>
2020-04-26 09:33:06 -0400

    use more pos than neg

--- opencv_objdetect/train.sh
+++ opencv_objdetect/train.sh
@@ -6,4 +6,4 @@ source utils.sh
 
 print_step "Training"
 
-opencv_traincascade -data $DATA_DIR -vec $VEC_FILE -bg $BG_FILE -numPos 250 -numNeg 500 -numStages 10 -w $WIDTH -h $HEIGHT
+opencv_traincascade -data $DATA_DIR -vec $VEC_FILE -bg $BG_FILE -numPos 500 -numNeg 250 -numStages 10 -w $WIDTH -h $HEIGHT
