bb5937fa3225b380fad6034bb20e9ce150d2f214
Harris Pittinsky <harris@pittinsky.com>
2020-04-26 14:44:14 -0400

    Update train.sh to not have hardcoded file dirs and names

--- opencv_objdetect/train.sh
+++ opencv_objdetect/train.sh
@@ -6,4 +6,14 @@ source utils.sh
 
 print_step "Training"
 
-opencv_traincascade -data $DATA_DIR -vec $VEC_FILE -bg $BG_FILE -numPos 500 -numNeg 250 -numStages 10 -w $WIDTH -h $HEIGHT
+cd $SAMPLES_DIR
+
+opencv_traincascade \
+  -data $(basename $DATA_DIR) \
+  -vec $VEC_FILE \
+  -bg $(basename $BG_FILE) \
+  -numPos 900 \
+  -numNeg 450 \
+  -numStages 15 \
+  -w $WIDTH \
+  -h $HEIGHT
