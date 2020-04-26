6c577b020c5c4d9d4c1f9c650740ef08372a60f4
Harris Pittinsky <harris@pittinsky.com>
2020-04-26 09:48:34 -0400

    git pull before npm build

--- runserver.sh
+++ runserver.sh
@@ -12,7 +12,8 @@ if [ "$1" == "dev" ]; then
   export FLASK_ENV=development
   source venv/bin/activate && flask run
 elif [ "$1" == "prod" ]; then
+  git pull
   npm run build
   export FLASK_ENV=production
-  git pull && source venv/bin/activate && gunicorn -w 1 -t 10000000 --bind 0.0.0.0:5000 frontend.wsgi
+  source venv/bin/activate && gunicorn -w 1 -t 10000000 --bind 0.0.0.0:5000 frontend.wsgi
 fi
